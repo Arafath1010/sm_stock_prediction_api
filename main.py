5b5e0bfdab05e35cd611256bea7e5de8cd13d41b
Arafath <Arafath10@users.noreply.huggingface.co>
2024-05-05 04:10:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
 import os
 import io
-temp = open("t.txt","w")
-temp.write("aaaaaaaaaaaaa")
-temp.close()
-
-temp = open("t.txt","r")
+import requests
 
 app = FastAPI()
 
 
+@app.on_event("startup")
+@repeat_every(seconds=60 * 10)  # 1 hour
+def refresh_the_api():
+    
+    url = "https://research-project-h4fb.onrender.com/refresh_api"
+    
+    payload = {}
+    headers = {
+      'accept': 'application/json'
+    }
+    
+    response = requests.request("POST", url, headers=headers, data=payload)
+    
+    print(response.text)
+    return response.text
 
 
-@app.get("/sample")
+@app.get("/test")
 def read_root():
-    return {"message": str(temp.read())}
+    return {"message":"running"}
 
