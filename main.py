6122003c907ce5aa645984c7a2f3a297b2278667
Arafath <Arafath10@users.noreply.huggingface.co>
2024-05-18 08:59:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from io import StringIO
 import os
+import uuid
 
 from pandasai import SmartDataframe
 import pandas as pd
@@ -23,17 +24,32 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+import base64
+from PIL import Image
+from io import BytesIO
+
+def convert_image_to_base64(image_path):
+    with Image.open(image_path) as image:
+        buffered = BytesIO()
+        image.save(buffered, format="PNG")
+        img_bytes = buffered.getvalue()
+        img_base64 = base64.b64encode(img_bytes)
+        img_base64_string = img_base64.decode("utf-8")
+    return img_base64_string
+
 @app.post("/get_image_for_text")
 async def get_image_for_text(email,query,file: UploadFile = File(...)):
         print(file.filename)
         with open(email+".csv", "wb") as file_object:
             file_object.write(file.file.read())
+        uuid1 = uuid.uuid1()
         llm = OpenAI(api_token=secret,save_charts=True)
         try:
             df = pd.read_csv(email+".csv") 
             sdf = SmartDataframe(df, config={"llm": llm})
             sdf.chat(query)
             image_path = "exports/charts/temp_chart.png"  # Replace with your image's path
-            return FileResponse(image_path)
+            base64str = convert_image_to_base64(image_path)
+            return {"id":str(uuid1),"image":base64str}
         except:
             return "try again"
