ae5f2f2ef42284e08b73971917547239bdad196e
Arafath <Arafath10@users.noreply.huggingface.co>
2024-07-21 13:41:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import os
 import uuid,requests
 import data_collector as dc
 
-data,message = get_data(b_id=637,product_name = "RS 10 TOFFEE")
-
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -19,7 +17,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# main
+data,message = dc.get_data(b_id=637,product_name = "RS 10 TOFFEE")
+print(message)
 
 
 @app.post("/get_image_for_text")
-async def get_image_for_text(email,query,file: UploadFile = File(...)):
+async def get_image_for_text(b_id:int,product_name:str)
+    # main
+    data,message = dc.get_data(b_id = b_id , product_name = product_name)
+    if message=="done":
+      # Summarize the sales count per month
+      data['transaction_date'] = pd.to_datetime(data['transaction_date'])
+      data.set_index('transaction_date', inplace=True)
+      monthly_sales = data['sell_qty'].resample('M').sum().reset_index()
+      full_trend,forecasted_value,rounded_value = forecast(monthly_sales)
+      print(full_trend,forecasted_value,rounded_value)
+      return rounded_value
